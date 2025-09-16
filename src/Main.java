e8ce800ea2d8d04038a884a1188c6380825a9d67
Samuel Costa <samukaka1000@gmail.com>
2025-09-15 22:35:42 -0400

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,29 +10,31 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
-        List<ArrayList<Integer>> nums = new ArrayList<>();
         List<Integer> results = new ArrayList<>();
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String line;
+
+        line = reader.readLine(); // array size
+        int size = Integer.parseInt(line);
+        int[] numbers = new int[size];
         int count = 0;
 
         line = reader.readLine();
-        StringBuilder number = new StringBuilder();
-        for (int i = 0; i < line.length(); i++) {
-            if(line.charAt(i) == ' ' || i == 0) {
-                nums.add(new ArrayList<>());
-                if (!number.isEmpty())
-                    nums.get(count).add(Integer.parseInt(number.toString()));
-                number.delete(0, number.length());
+        StringBuilder buffer = new StringBuilder();
+
+        for (int i = 0; i < line.length()-1; ++i) {
+            while(i <= line.length()-1 && line.charAt(i) != ' ') {
+                buffer.append(line.charAt(i));
+                ++i;
             }
-            else
-                number.append(line.charAt(i));
+
+            numbers[count] = Integer.parseInt(buffer.toString());
+            buffer.delete(0, buffer.length());
+            ++count;
         }
 
-        System.out.println(nums);
-
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < numbers.length; i += 2) {
 
             int x = 0;
             int lastX = 1;
@@ -39,25 +42,34 @@ public class Main {
             int y = 1;
             int lastY = 0;
 
-            int a = nums.get(i).get(0);
-            int b = nums.get(i).get(1);
+            int a = numbers[i];
+            int b = numbers[i+1];
+            int origMod = b;
 
             while (b != 0) {
                 int quotient = a/b;
                 int temp = b;
                 b = a % b;
                 a = temp;
+
                 temp = x;
                 x = lastX - quotient * x;
                 lastX = temp;
+
                 temp = y;
                 y = lastY - quotient * y;
                 lastY = temp;
             }
 
-            results.add(lastX);
+            if (a != 1) {
+                results.add(null);
+            } else {
+                int inverse = (lastX % origMod + origMod) % origMod;
+                results.add(inverse);
+            }
         }
 
         System.out.println(results);
+
     }
 }
